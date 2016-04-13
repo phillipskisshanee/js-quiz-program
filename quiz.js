cee82332b7c9c4850cfa5055596d6d2d1cf24f02
phillipskisshanee <phillips.kisshanee@wustl.edu>
2016-04-13 18:07:35 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,15 +2,59 @@
 var correct = 0;
 
 // prompt
-var answer1 = prompt("ask a question...");
+var answer1 = prompt("When will Frank Ocean release his second album?");
 // console.log("the answer is:" + answer1);
 
 // if statement
-if ( answer1.toUpperCase() === 'MAGIC' ) {
+if ( answer1.toUpperCase() === 'never' ) {
+ correct += 1;
+}
+
+//console.log("correst:" + correct);
+
+// prompt
+var answer1 = prompt("Did you believe Frank Ocean when he said he was coming out with new music?");
+// console.log("the answer is:" + answer1);
+
+// if statement
+if ( answer1.toUpperCase() === 'nope' ) {
+ correct += 1;
+}
+
+//console.log("correst:" + correct);
+
+// prompt
+var answer1 = prompt("How many times has Frank lied to us about releasing new music?");
+// console.log("the answer is:" + answer1);
+
+// if statement
+if ( answer1.toUpperCase() === 'too many' ) {
+ correct += 1;
+}
+
+//console.log("correst:" + correct);
+
+// prompt
+var answer1 = prompt("Has Frank forgotten about us?");
+// console.log("the answer is:" + answer1);
+
+// if statement
+if ( answer1.toUpperCase() === 'definitely' ) {
+ correct += 1;
+}
+
+//console.log("correst:" + correct);
+
+// prompt
+var answer1 = prompt("Is Frank Ocean still alive?");
+// console.log("the answer is:" + answer1);
+
+// if statement
+if ( answer1.toUpperCase() === 'questionable' ) {
  correct += 1;
 }
 
 //console.log("correst:" + correct);
 
 // output results
-document.write("<p>" + answer1 + "</p>");
+document.write("<p>" Congrats! If you believed Frank was going to grace us with new music, you played yourself. "</p>");
