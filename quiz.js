aaac19b61f3f95d2ae07a491e57c8a117fad1874
phillipskisshanee <phillips.kisshanee@wustl.edu>
2016-04-13 18:17:42 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -3,46 +3,33 @@ var correct = 0;
 
 // prompt
 var answer1 = prompt("When will Frank Ocean release his second album?");
+var answer2 = prompt("Did you believe Frank Ocean when he said he was coming out with new music?");
+var answer3 = prompt("How many times has Frank lied to us about releasing new music?");
+var answer4 = prompt("Has Frank forgotten about us?");
+var answer5 = prompt("Is Frank Ocean still alive?");
 
 // if statement
 if ( answer1.toUpperCase() === 'never' ) {
  correct += 1;
 }
 
-// prompt
-var answer2 = prompt("Did you believe Frank Ocean when he said he was coming out with new music?");
-
-// if statement
 if ( answer2.toUpperCase() === 'nope' ) {
  correct += 1;
 }
 
-// prompt
-var answer3 = prompt("How many times has Frank lied to us about releasing new music?");
-
-// if statement
 if ( answer3.toUpperCase() === 'too many' ) {
  correct += 1;
 }
 
-
-// prompt
-var answer4 = prompt("Has Frank forgotten about us?");
-
-// if statement
 if ( answer4.toUpperCase() === 'definitely' ) {
  correct += 1;
 }
 
-
-// prompt
-var answer5 = prompt("Is Frank Ocean still alive?");
-// console.log("the answer is:" + answer1);
-
-// if statement
 if ( answer5.toUpperCase() === 'questionable' ) {
  correct += 1;
 }
+
+// console.log("the answer is:" + answer1);
 
 
 // output results
