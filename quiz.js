38ab73b6da66c6a6adb46649a048114220fee23f
phillipskisshanee <phillips.kisshanee@wustl.edu>
2016-04-13 17:58:55 -0500

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -3,11 +3,14 @@ var correct = 0;
 
 // prompt
 var answer1 = prompt("ask a question...");
+// console.log("the answer is:" + answer1);
 
 // if statement
 if ( answer1.toUpperCase() === 'MAGIC' ) {
  correct += 1;
 }
 
+//console.log("correst:" + correct);
+
 // output results
-document.write("<p>write something to page...</p>");
+document.write("<p>" + answer1 + "</p>");
